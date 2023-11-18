979d8d55b81f3eb102df7394eac540f6555191e2
SnehPatel07 <99879133+SnehPatel07@users.noreply.github.com>
2023-11-18 10:04:05 +0530

    Calender.py

--- Calender.py
+++ Calender.py
@@ -1,9 +1,5 @@
-from datetime import date
- 
-def age(birthdate):
-    today = date.today()
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    return age
+import calender
 
-
-age(birthdate)
+year = int(input("Enter the Year : ")
+c = calender.calender(year)
+print(c)
